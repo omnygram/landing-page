db1b4d056424e85adbd6e06a88da06060c9b005e
hskang9 <hyungsuk@aurora.dev>
2023-06-25 02:14:21 +0900

    adjust margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ export default function Home() {
           <div className="w-[600px] flex flex-col items-center align-center">
             <div className="flex flex-col items-center justify-center">
               <img src="/omnygram.svg" className="w-[200px]" />
-              <div className="text-center space-y-6 pt-6">
+              <div className="text-center space-y-6 pt-12">
                 <h1 className="text-[36px] text-[#4d4d4d] font-medium font-sans">Omnygram</h1>
                 <h2 className="text-[20px] text-[#8c8c8c] font-light font-sans">A new era of cross chain</h2>
               </div>
             </div>
-            <div className="flex  flex-row justify-between pt-[64px]">
+            <div className="flex  flex-row justify-between pt-12">
               <a href="https://docsend.com/view/e8cvqtwzz9u7jpd2">
                 <div className="w-[200px] text-center p-4 font-sans text-[18px]">Pitch deck</div>
               </a>
