16ac60395d2066930c9c98b5c85077d7d437c756
hskang9 <hyungsuk@aurora.dev>
2023-06-25 22:57:53 +0900

    adjust margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
               <a href="https://docsend.com/view/e8cvqtwzz9u7jpd2">
                 <div className="w-[200px] text-center p-4 font-sans text-[18px]">Pitch deck</div>
               </a>
-
               <a href="https://docsend.com/view/p7jckm753kib9j23">
                 <div className="w-[200px] text-center p-4 font-sans text-[18px]">Paper</div>
               </a>
